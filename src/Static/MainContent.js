19984c2a3bd743e47ec088eb6281d86afde6c086
venkatasivaramakrishna_movva <venkatasivaramakrishna.movva@techgrit.com>
2021-06-15 03:17:15 +0530

    updated my profile content

--- src/Static/MainContent.js
+++ src/Static/MainContent.js
@@ -3,11 +3,31 @@ function MainContent() {
         < div className="content">
             <h1 style={{ color: "Grey" }}>About me</h1>
 
-            <p>I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web development and print media. I enjoy turning complex problems into simple, beautiful and intuitive designs.</p>
-
-            <p>My job is to build your website so that it is functional and user-friendly but at the same time attractive. Moreover, I add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring across your message and identity in the most creative way. I created web design for many famous brand companies.</p>
+            <p>I am a <b>Technical Architect</b> with 16+ years of experience on 20+ enterprise-grade software projects
+            from various functional domains including HealthCare, Financial Services, Logistics, Banking, etc...</p>
 
             <h1 style={{ color: "Grey" }}>What I'm Doing</h1>
+
+            <p> Presales, Architect, Solution, Design, Develop, Maintain, Mentor, Ownership, Lead, Talent Acquisition</p>
+
+            <h1 style={{ color: "Grey" }}>Tech Stacks</h1>
+
+            <p> Microsoft, MERN, MEAN, Cloud (AWS, Azure), PYTHON, MS SQL, Postgress, HASURA, GraphQL, Docker,
+            Microservices, WebRTC, Twilio, Xstate, ASP.NET, .net core, Linux Deployments, EF core, Java, Spring Boot,
+            SyncFusion, DevOps, Ansible, Git, Bit Bucket, Source Tree, Ionic-Capacitor, Flutter, Dart, Android, IOS (not much)</p>
+
+            <h1 style={{ color: "Grey" }}>On AWS</h1>
+
+            <p> Route53, Cloud Front, ACM, Secrets Manager, S3, Load Balancers, Auto Scaling,
+            EC2, ECS, Fargate, EFS, RDS, Aurora, Cognito, Pinpoint, SNS, SES, AWS Connect (Cotact Centre),
+            Kinesis, Kinesis Signal Channels, CloudWatch, IAM, Code Commit, ELB,
+            Code Build, Code Deploy, Code Pipeline, Lambda, API Gateway, Dynamo DB, Document DB,
+            Database Migration Services, Billing Optimization, Cloud Formation, Textract </p>
+
+            <h1 style={{ color: "Grey" }}>On Azure</h1>
+
+            <p> Active Directory Services, Power BI, Share point </p>
+
         </div >
     );
 }
--- src/Static/SideContent.js
+++ src/Static/SideContent.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCloudDownloadAlt } from '@fortawesome/free-solid-svg-icons'
+
 function SideContent() {
   return (
     < div className="sidebar" style={{
@@ -5,15 +8,22 @@ function SideContent() {
       fontWeight: "bold", alignItems: "center"
     }}>
       <div style={{ paddingTop: "20px" }}>
-        <img src={process.env.PUBLIC_URL + "/Assets/Myphoto.jpg"}></img>
+        <img src={process.env.PUBLIC_URL + "/Assets/Myphoto.jpg"} alt=""></img>
       </div>
       <p style={{ color: "#44566C", marginBlockStart: "auto" }}>SivaRamaKrishna Movva</p>
       <div style={{
         position: "relative", borderRadius: "20px", backgroundColor: "#E9EDF0",
         color: "grey", display: "inline-block", paddingLeft: "20px",
         paddingRight: "20px", marginBlockStart: "auto"
-      }}>Architect</div>
+      }}>Technical Architect</div>
       <p style={{ position: "relative", color: "grey" }}>+91 9845785754</p>
+
+      <div style={{ position: "relative", display: "inline-block" }}>
+        <a style={{ color: "blue" }} href="/Resume.htm">Resume</a>&nbsp;&nbsp;
+        <a href={process.env.PUBLIC_URL + "/Assets/Resume.doc"}>
+          <FontAwesomeIcon icon={faCloudDownloadAlt} size="xs" />
+        </a>
+      </div>
 
     </div >
   );
