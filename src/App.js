82e2fcb05336c0ba706f3e622d30e815280c6a83
venkatasivaramakrishna_movva <venkatasivaramakrishna.movva@techgrit.com>
2021-06-22 11:22:36 +0530

    Lazy loading enabled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import React, { Suspense } from 'react';
 import './App.css';
-import SideContent from './Static/SideContent'
-import MainContent from './Static/MainContent'
-import Portfolio from './Static/Portfolio'
-import Contact from './Static/Contact'
-import Menubar from './Static/Menubar'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+const SideContent = React.lazy(() => import('./Static/SideContent'));
+const MainContent = React.lazy(() => import('./Static/MainContent'));
+const Portfolio = React.lazy(() => import('./Static/Portfolio'));
+const Contact = React.lazy(() => import('./Static/Contact'));
+const Menubar = React.lazy(() => import('./Static/Menubar'));
 
 function App() {
   return (
     <Router>
-      <Menubar />
-      <SideContent />
+      <Suspense fallback={<div>loading..</div>}>
+        <Menubar />
+      </Suspense>
+      <Suspense fallback={<div>loading..</div>}>
+        <SideContent />
+      </Suspense>
       <Switch>
         <Route exact path="/">
-          <MainContent />
+          <Suspense fallback={<div>loading..</div>}>
+            <MainContent />
+          </Suspense>
         </Route>
         <Route path="/portfolio">
-          <Portfolio />
+          <Suspense fallback={<div>loading..</div>}>
+            <Portfolio />
+          </Suspense>
         </Route>
         <Route path="/contact">
-          <Contact />
+          <Suspense fallback={<div>loading..</div>}>
+            <Contact />
+          </Suspense>
         </Route>
       </Switch>
     </Router>
